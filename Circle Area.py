b3fc58b6d85fa217c0610ddddc345827a78857c6
KOVVURI VEERA VENKATA SURENDER <88876517+SurenderKovvuri@users.noreply.github.com>
2023-02-09 15:19:25 +0530

    Circle area
    Calculate Circle area by taking Radius as input

---
+++ Circle Area.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from math import pi
+r = float(input("Input the radius of the cricle: "))
+CircleArea = pi*(r**2)
+print("The area of the circle with the radius " + str(r) + " is: " + str(CircleArea))
+
+
+# In[ ]:
+
+
+
+
