9b5c43139b48f4839ad9ef0111cb3e0418650092
KOVVURI VEERA VENKATA SURENDER <88876517+SurenderKovvuri@users.noreply.github.com>
2023-02-09 15:31:17 +0530

    Filenam extension

---
+++ Filename Extension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+filename = input("Input the Filename: ")
+extension = filename.split(".")
+print("The extension of the file is: " + extension[-1])
+
+
+# In[ ]:
+
+
+
+
